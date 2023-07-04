3d92391ccf66a8d89a6964288ba4773028fcc675
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-07-04 20:43:14 +0530

    Add create surfaces

--- board.py
+++ board.py
@@ -3,3 +3,14 @@ from square import Square
 from piece import *
 from move import Move
 from tkinter import *
+
+# global textures
+textures={}
+
+def create_surfaces():
+    for color in ['white','black']:
+        textures[color]={}
+        for image in ['rook','bishop','queen','king','knight','pawn']:
+            path=os.path.join('pieces',color,f'{image}.png')
+            surface=pygame.image.load(path).convert_alpha()
+            textures[color][image]=surface
