780e8d4d4a39673b6c94c599371c044d4a9c6622
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-07-09 20:34:32 +0530

    Add add pieces

--- board.py
+++ board.py
@@ -20,8 +20,40 @@ class Board:
         f=open('settings.json')
         self.settings=json.load(f)
         del f
+        create_surfaces()
         
     def _create(self):
         for row in range(self.settings['ROWS']):
             for col in range(self.settings['COLS']):
                 self.squares[row][col]=Square(row, col)
+
+    def _add_pieces(self,color):
+        if self.down=='white':
+            (pawn_piece,other_piece)=(6,7) if color=='white' else (1,0)
+        else:
+            (pawn_piece,other_piece)=(1,0) if color=='white' else (6,7)
+
+        # pawns
+        for col in range(self.settings['COLS']):
+            self.squares[pawn_piece][col]=Square(pawn_piece,col,Pawn(color,textures[color]['pawn'].get_rect(),self.down))
+
+        # knights
+        self.squares[other_piece][1]=Square(other_piece, 1,Knight(color,textures[color]['knight'].get_rect()))
+        self.squares[other_piece][6]=Square(other_piece,6,Knight(color,textures[color]['knight'].get_rect()))
+
+        # Bishops
+        self.squares[other_piece][2]=Square(other_piece, 2,Bishop(color,textures[color]['bishop'].get_rect()))
+        self.squares[other_piece][5]=Square(other_piece, 5,Bishop(color,textures[color]['bishop'].get_rect()))
+
+        # Rooks
+        self.squares[other_piece][0]=Square(other_piece, 0,Rook(color,textures[color]['rook'].get_rect()))
+        self.squares[other_piece][7]=Square(other_piece, 7,Rook(color,textures[color]['rook'].get_rect()))
+
+        # Queen
+        col=3 if self.down=='white' else 4
+        self.squares[other_piece][col]=Square(other_piece, col,Queen(color,textures[color]['queen'].get_rect()))
+
+        # King
+        col=4 if self.down=='white' else 3
+        self.squares[other_piece][col]=Square(other_piece, col,King(color,textures[color]['king'].get_rect()))
+    
