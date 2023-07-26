91be79012640569a5c6efd9351e9a9cb252e755d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-07-26 18:37:06 +0530

    Initial commit

--- square.py
+++ square.py
@@ -6,3 +6,13 @@ class Square:
         self.col=col
         self.piece=piece
         self.rect=pygame.rect.Rect(61+self.col*80,40+self.row*80,80,80)
+    
+    def has_piece(self) -> bool:
+        return self.piece!=None
+    
+    @staticmethod
+    def inrange(*args) -> bool:
+        for arg in args:
+            if arg<0 or arg>7:
+                return False
+        return True
